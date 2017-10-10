b76d6fc6a2690c526545dc5e3516a678c8bd4463
mkamedula <malgorzata.kamedula@gmail.com>
2017-10-10 12:55:14 +0200

    remove build folder

--- controllers/rt_plugins/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user/malgorzata/workspace/src/controllers/rt_plugins
-# Build directory: /home/user/malgorzata/workspace/src/controllers/rt_plugins/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
--- controllers/rt_plugins/build/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/user/malgorzata/workspace/src/controllers/rt_plugins/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
