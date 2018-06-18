ab1b14e0b25a3c221e4942c5454fd2896b4c5236
mkamedula <malgorzata.kamedula@gmail.com>
2018-06-18 19:57:44 +0200

    Pseudo Inverse damping a vector
    Change pseudo inverse damping to vector to allow different values for each row

--- locomotion_framework/utils/eigen_utils/src/eigen_utils.cpp
+++ locomotion_framework/utils/eigen_utils/src/eigen_utils.cpp
@@ -1,10 +1,49 @@
 #include "mwoibn/eigen_utils/eigen_utils.h"
-
+#include <functional>
 
 Eigen::Matrix<bool,Eigen::Dynamic,1> mwoibn::eigen_utils::flip(Eigen::Matrix<bool,Eigen::Dynamic,1> vector){
 
-  for(int i = 0; i < vector.size(); i++)
-    vector[i] = !vector[i];
+        for(int i = 0; i < vector.size(); i++)
+                vector[i] = !vector[i];
 
-  return vector;
+        return vector;
 }
+
+void mwoibn::eigen_utils::limit2PI(double ref, double& st){
+        if(st - ref > 1.57079632) {
+//    std::cout << "\t" << ref << "\t" << st << "\t";
+
+                st -= 3.14159265;
+//  std::cout << st << std::endl;
+                limit2PI(ref, st);
+        }
+        else if (ref - st > 1.57079632) {
+//    std::cout << "\t" << ref << "\t" << st << "\t";
+                st += 3.14159265;
+//    std::cout << st << std::endl;
+                limit2PI(ref, st);
+        }
+}
+
+void mwoibn::eigen_utils::limitPI(double ref, double& st){
+        if(st - ref > 0.78539816) {
+//    std::cout << "\t" << ref << "\t" << st << "\t";
+
+                st -= 1.57079632;
+//  std::cout << st << std::endl;
+                limitPI(ref, st);
+        }
+        else if (ref - st > 0.78539816) {
+//    std::cout << "\t" << ref << "\t" << st << "\t";
+                st += 1.57079632;
+//    std::cout << st << std::endl;
+                limitPI(ref, st);
+        }
+}
+
+// std::size_t mwoibn::eigen_utils::hash(Eigen::Matrix<bool,Eigen::Dynamic,1> vec){
+//
+//         std::vector<bool> std_vec(vec.data(), vec.data() + vec.size());
+//         return std::hash<std::vector<bool> >{} (std_vec);
+//
+// }
