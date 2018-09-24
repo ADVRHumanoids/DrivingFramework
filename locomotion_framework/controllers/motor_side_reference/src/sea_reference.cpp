ba60b386f8695db54cc2aa4ce1c4f9597af3c725
mkamedula <malgorzata.kamedula@gmail.com>
2018-09-24 13:38:56 +0200

    Add missing damping in compliance compensation

--- locomotion_framework/controllers/motor_side_reference/src/sea_reference.cpp
+++ locomotion_framework/controllers/motor_side_reference/src/sea_reference.cpp
@@ -7,42 +7,42 @@ namespace motor_side_reference
 {
 
 SeaReference::SeaReference(
-    mwoibn::robot_class::Robot& robot,
-    mwoibn::basic_controllers::LowerLevelController& gravity_compensation,
-    mwoibn::robot_class::INTERFACE interface)
-    : mwoibn::basic_controllers::LowerLevelController(robot, interface),
-      _gravity_compensation(gravity_compensation)
+        mwoibn::robot_class::Robot& robot,
+        mwoibn::basic_controllers::LowerLevelController& gravity_compensation,
+        mwoibn::robot_class::INTERFACE interface)
+        : mwoibn::basic_controllers::LowerLevelController(robot, interface),
+        _gravity_compensation(gravity_compensation)
 {
-  _command.resize(_robot.getDofs());
-  _stiffness_matrix_inverser.init(_robot.actuators().getStiffnessMatrix());
-  updateStiffnessMatrix();
+        _command.resize(_robot.getDofs());
+        _stiffness_matrix_inverser.init(_robot.actuators().getStiffnessMatrix(), 1e-10);
+        updateStiffnessMatrix();
 }
 
 void SeaReference::updateStiffnessMatrix()
 {
-  _stiffness_matrix_inverser.compute(_robot.actuators().getStiffnessMatrix());
+        _stiffness_matrix_inverser.compute(_robot.actuators().getStiffnessMatrix());
 
-  //  _selection = _robot.actuators().getActuationTypes(
-  //      {mwoibn::robot_class::ACTUATOR_TYPE::ELASTIC});
+        //  _selection = _robot.actuators().getActuationTypes(
+        //      {mwoibn::robot_class::ACTUATOR_TYPE::ELASTIC});
 
-  _stiffness_matrix = _stiffness_matrix_inverser.get();
+        _stiffness_matrix = _stiffness_matrix_inverser.get();
 }
 
 void SeaReference::compute()
 {
 
-  _command = _stiffness_matrix * _gravity_compensation.getCommand() +
-             _robot.command.get(mwoibn::robot_class::INTERFACE::POSITION);
+        _command = _stiffness_matrix * _gravity_compensation.getCommand() +
+                   _robot.command.get(mwoibn::robot_class::INTERFACE::POSITION);
 }
 
 const mwoibn::VectorN& SeaReference::update()
 {
 
-  _gravity_compensation.update();
-  compute();
-  setCommand();
+        _gravity_compensation.update();
+        compute();
+        setCommand();
 
-  return getCommand();
+        return getCommand();
 }
 } // namespace package
 } // namespace library
