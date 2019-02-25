65a0e3e5b84471a8be24543ef07a69f52d6440fc
mkamedula <malgorzata.kamedula@gmail.com>
2019-02-25 14:28:19 +0100

    Add robot point for point/frame orientation

---
+++ locomotion_framework/models/robot_class/include/mwoibn/robot_points/frame_orientation.h
@@ -0,0 +1,37 @@
+#ifndef __MWOIBN__ROBOT_CLASS__FRAME_ORIENTATION_H
+#define __MWOIBN__ROBOT_CLASS__FRAME_ORIENTATION_H
+
+#include "mwoibn/robot_points/state.h"
+#include "mwoibn/point_handling/frame.h"
+
+
+namespace mwoibn
+{
+namespace robot_points
+{
+
+class FrameOrientation: public State
+{
+
+public:
+  template<typename Body>
+  FrameOrientation(Body body, mwoibn::robot_class::Robot& robot): State(robot.getModel(), robot.state), point(body, robot.getModel(), robot.state){
+    _point.setZero(3);
+  }
+
+  using Point::operator=;
+
+
+  void compute(){ mwoibn::std_utils::notImplemented(__PRETTY_FUNCTION__);}
+
+  void computeJacobian() {_jacobian.noalias() = point.getOrientationJacobian();}
+
+  mwoibn::point_handling::Frame point;
+
+
+};
+
+} // namespace package
+} // namespace library
+
+#endif // CENTER_OF_MASS_H
