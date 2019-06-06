9c58282bbac412b0789037afc0802194f9225ac2
mkamedula <malgorzata.kamedula@gmail.com>
2019-06-06 19:58:49 +0200

    Update state estimation to estimate heading from velocity estimation

---
+++ controllers/nrt_software/src/plugins/odometry_4.cpp
@@ -0,0 +1,6 @@
+#include "mgnss/nrt_software/plugins/odometry_4.h"
+#include "mgnss/plugins/ros_base.h"
+#include "mgnss/plugins/xbot_base_v2.h"
+
+MGNSS_REGISTER_ROS_PLUGIN_(mgnss::nrt_software::plugins::Odometry4)
+MGNSS_REGISTER_XBOT_PLUGIN_(mgnss::nrt_software::plugins::Odometry4)
