6bd9b9d2d57736a84dc31098b9f45fc1051e7ae8
mkamedula <malgorzata.kamedula@gmail.com>
2019-10-02 15:49:01 +0200

    Add comments to some robot point source code files

--- locomotion_framework/models/robot_class/include/mwoibn/robot_points/handler.h
+++ locomotion_framework/models/robot_class/include/mwoibn/robot_points/handler.h
@@ -10,8 +10,12 @@ namespace mwoibn
 namespace robot_points
 {
 
-/** @brief Keeps all the contact data and probides acces to individual contacts
- * **/
+//! This class provides a methods to menage multiple Robot Points.
+/**
+ *  This class provides a convinent and real-time safe way to compute the concatenatd vectors and Jacobians for multiple robot points.
+ *  This class provides iterators to provide access to the Type interface for all the elements
+ *  This class allows to menage updates for multiple robot points
+ */
  template<typename Type>
 class Handler
 {
@@ -22,6 +26,7 @@ public:
     Handler() : _dofs(0) {}
 
 
+    // A move constructor
     template<typename Other>
     Handler(Other&& other)
           : _positions(other.getState()), _jacobian(other.getJacobian()), _dofs(other._dofs)
@@ -36,6 +41,7 @@ public:
 
     virtual ~Handler() { }
 
+    //! add a point to the Handler by moving a unique pointer
     template<typename Other>
     int add(std::unique_ptr<Other> point)
     {
@@ -44,12 +50,7 @@ public:
             return _points.size()-1;
     }
 
-    // template<typename Other>
-    // void add(Other&& point)
-    // {
-    //        _points.push_back(std::unique_ptr<Other>(new Other(point)));
-    //        resize();
-    // }
+    //! add a point to the Handler with a copy constructor
 
     template<typename Other>
     int add(Other point)
@@ -59,19 +60,14 @@ public:
            return _points.size()-1;
     }
 
-    // template<typename Other>
-    // void add(const Other& point)
-    // {
-    //        _points.push_back(std::unique_ptr<Other>(new Other(point)));
-    //        resize();
-    // }
+    //! allocate the memmory according to the current handler state
 
     virtual void resize(){
           	double size = 0, rows = 0, cols = 0;
           	for(auto& point: _points){
               		  size += point->size();
           		      rows += point->rows();
-                    cols = point->cols();
+             cols = point->cols(); // the number of columns has to be equal for all the points
           	}
           	_jacobian = mwoibn::Matrix::Zero(size, cols);
             _positions = mwoibn::VectorN::Zero(rows);
@@ -79,25 +75,22 @@ public:
     }
 
 
-    /**
-     * @brief Gives number of all considered contact points
-     *
-     */
+    //! return the number of points in the Handler
     int size() const {
             return _points.size();
-    }                                               // RT?
+    }                                             
 
+    //! update all the points
+    /** @param[in] - should the jacobians (mappings) be updated as well */
     void update(bool jacobian){
     	for(auto& point: _points)
     		point->update(jacobian);
     }
 
+    //! remove an i_th point from the Handler
     bool remove(int i)
     {
-      // std::cout << __PRETTY_FUNCTION__ << std::endl;
-      // std::cout << i << std::endl;
-      // std::cout << _points.size() << std::endl;
-      // std::cout << (std::abs(i) <= _points.size()) << std::endl;
+
       if(i < 0 && std::abs(i) <= _points.size()){
           _points.erase(_points.end() - i);
           resize();
@@ -113,10 +106,12 @@ public:
       return false;
     }
 
+    //! remove all the points from the Handler
     void clear(){
       _points.clear();
     }
 
+    //! provide an access to the n-th robot point, a zero-based 
     Type& point(unsigned int id)
     {
       if (id < _points.size())
@@ -125,6 +120,7 @@ public:
         throw std::out_of_range("Given ID is beyond a vector scope");
     }
 
+    //! Computes and returns a concatenated matrix of jacobians of all the points in the Handler. This method is RT safe
     const mwoibn::Matrix& getJacobian()
     {
       _jacobian.setZero();
@@ -140,10 +136,13 @@ public:
       return _jacobian;
     }
 
+    //! Returns the concatenated matrix of jacobians of all the points in the Handler. This method is RT safe
     const mwoibn::Matrix& jacobian() const {return _jacobian;}
+    //! Returns the concatenated vector of vectors of all the points in the Handler. This method is RT safe
     const mwoibn::VectorN& state() const {return _positions;}
 
 
+    //! Returns the vector of jacobians of all the points. WARNING: This method is not RT safe
     std::vector<mwoibn::Matrix> getJacobians()
     {
 
@@ -156,6 +155,7 @@ public:
       return jacobians;
     }
 
+    //! Computes and returns the concatenated vector  of vectors of all the points in the Handler. This method is RT safe
     const mwoibn::VectorN& getState()
     {
       _positions.setZero();
@@ -171,6 +171,7 @@ public:
       return _positions;
     }
 
+    //! Returns the vector of vectors of all the points in the Handler. WARNING: This method is not RT safe
     std::vector<mwoibn::VectorN> getStates()
     {
       std::vector<mwoibn::VectorN> positions;
@@ -184,6 +185,7 @@ public:
 
 
 
+    //! returns the number of rows in the concatenated jacobian
     int rows() const {
             int i = 0;
             for (auto& point : _points)
@@ -191,31 +193,37 @@ public:
             return i;
     }
 
+    //! returns the number of columns in the concatenated jacobian
     int cols() const {
             return _dofs;
     }
 
+    //! An iterator to the beginning of the robot points container
     typename std::vector<std::unique_ptr<Type>>::iterator begin(){return _points.begin();}
+    //! An iterator to the end of the the robot points container
     typename std::vector<std::unique_ptr<Type>>::iterator end(){return _points.end();}
 
+    //! A constant iterator to the beginning of the robot points container
     typename std::vector<std::unique_ptr<Type>>::const_iterator begin() const {return _points.begin();}
+    //! A constant iterator to the end of the robot points container
     typename std::vector<std::unique_ptr<Type>>::const_iterator end() const {return _points.end();}
 
+    //! A method to provide a zero-based access to the n-th last robot point
     virtual Type& end(unsigned int i) {
             int idx = -i - 1;
-            // std::cout << "idx\t" << idx << std::endl;
             return *(_points.end()[idx]);
     }
 
+    //! A method to provide a zero-based access to the n-th robot point
     virtual Type& operator[](int i) {
             return *_points[i];
     }
 
 protected:
-    std::vector<std::unique_ptr<Type> > _points;
-    mwoibn::Matrix _jacobian;
-    mwoibn::VectorN _positions;
-    unsigned int _dofs;
+    std::vector<std::unique_ptr<Type> > _points; // container to keep the robot_points
+    mwoibn::Matrix _jacobian; // concatenation of all the jacobians
+    mwoibn::VectorN _positions; // concatenation of all the vectors
+    unsigned int _dofs; // number of decalred columns
 };
 
 
--- locomotion_framework/models/robot_class/include/mwoibn/robot_points/point.h
+++ locomotion_framework/models/robot_class/include/mwoibn/robot_points/point.h
@@ -9,12 +9,21 @@ namespace mwoibn
 namespace robot_points
 {
 
+// Forward declaration of the Rotation class
 class Rotation;
 
+//! This class provides a common interface to impelment different robot characteristics 
+/** A robot point is defined as any element that can be defined by the vector value and a matrix mapping
+ *
+ */
 class Point
 {
 
 public:
+  //! Constructor
+  /** @param[in] state - size of a state vector, number of rows in a mapping matrix
+   *  @param[in] dofs - number of columns in a mapping matrix
+   */
   Point(unsigned int state, unsigned int dofs){
     _jacobian.setZero(state, dofs);
     _point.setZero(state);
@@ -29,19 +38,28 @@ public:
 
   virtual ~Point() {}
 
+  //! update a point state (vector)
   virtual void compute() = 0;
 
+  //! update a point mapping (matrix)
   virtual void computeJacobian() = 0;
 
+  //! update a robot point
+  /** @param[in] jacobain - should the matrix be updated */
   virtual void update(bool jacobian = true);
 
+  //! Returns the reference to the mapping matrix
   const mwoibn::Matrix& getJacobian() const {return _jacobian;}
 
+  //! Returns the reference to the vector (value)
   const mwoibn::VectorN& get() const {return _point;}
 
+  //! Returns the size of the value vector
   int size(){return _point.size();}
 
+  //! Returns the number of rows in a matrix
   int rows() {return _jacobian.rows();}
+  //! Returns the number of columns in a matrix
   int cols() {return _jacobian.cols();}
 
   virtual Point& operator=(const Point& other);
@@ -54,10 +72,13 @@ public:
 
   Point& operator-=(const Point& other);
 
+  //! multiply the matrix by a scalar factor
   void multiplyJacobian(double factor);
 
+  //! apply the rotation matrix to the point
   void rotateFrom(mwoibn::robot_points::Rotation& rotation, bool jacobian = false);
 
+  //! apply the inverse rotation matrix to the point 
   void rotateTo(mwoibn::robot_points::Rotation& rotation, bool jacobian = false);
 
 protected:
@@ -70,4 +91,4 @@ protected:
 } // namespace package
 } // namespace library
 
-#endif // CENTER_OF_MASS_H
+#endif // __MWOIBN__ROBOT_POINTS__POINT_H
